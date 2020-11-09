0b06b5bb25c1fcc5bb1a3422179cc5a5647042d6
Douglas Ramos <douglasramospimenta@gmail.com>
2020-11-09 19:55:13 -0300

    corrigindo bug git

--- App/Controllers/Index.php
+++ App/Controllers/Index.php
@@ -27,26 +27,33 @@ class Index extends Action
 
 		$this->view->taskList = $this->task->fetchAll();
 		TaskFormat::taskListFormat($this->view->taskList);
-		
+
+		$this->form->setAction('index');
+		$this->view->form = $this->form;
+		//formatar os dados das tarefas.
 		$this->render('index');
 	}
 
 	public function add()
 	{
 		$addResult = TaskResult::ADD_OK;
-		if($this->task->add($_POST) !== true) {
+		//receber os dados do formulários
+		if($this->task->add($_POST) !== true)
+		{
 			$addResult = TaskResult::ADD_ERROR;
-		}
-		
+		};
+
 		$this->redirect("/index/{$addResult}");
+
+		//$this->render('add');
+		//$this->redirect();
 	}
 
-
 	public function edit()
-	{
-		
+    {
+
 		$this->view->taskEdit = $this->task->find($this->getParam());
-		
+
 		if($this->view->taskEdit == false) {
 			$this->redirect("/index");
 		}
@@ -54,41 +61,33 @@ class Index extends Action
 		$this->form->setAction('edit');
 		$this->view->form = $this->form;
 
-		 
 		$this->render('edit');
 	}
 	
+
 	public function update()
 	{
 		$editResult = TaskResult::EDIT_OK;
-		if($this->task->update($_POST, $_POST['id']) !== 1) {
+		if($this->task->update($_POST, $_POST['id']) !== 1) 
+		{
 			$editResult = TaskResult::EDIT_ERROR;
 		}
 		
 		$this->redirect("/index/{$editResult}");
 	}
 
+
+
 	public function delete()
 	{
-		$deleteResult = TaskResult:: DELETE_OK;
-		if($this->task->delete($_POST) !== true) {
+
+		$deleteResult = TaskResult::DELETE_OK;
+		if ($this->task->delete($_POST) !== true) {
 			$deleteResult = TaskResult::DELETE_ERROR;
 		}
-		
-		$this->redirect("/index/{$deleteResult}");
-	
-	}
 
-	public function edit()
-	{
-		
-		var_dump($this->getParam());
-	}
-	
-	public function delete()
-	{
-		var_dump($this->getParam());
-	
+		$this->redirect("/index/{$deleteResult}");
+
 	}
 	
 }
