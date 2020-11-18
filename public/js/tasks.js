493608215f23ccc389795ec0671bdf7a88870bfe
Douglas Ramos <douglasramospimenta@gmail.com>
2020-11-18 18:57:38 -0300

    Atividade - 011

---
+++ public/js/tasks.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	$('a[data-confirm]').click(function(ev){
+		var href = $(this).attr('href');
+		var text = $(this).attr('data-confirm');
+		if(!$('#confirm-delete').length) {
+			$('body').append('<div class="modal fade" id="confirm-delete" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true"><div class="modal-dialog" role="document"><div class="modal-content"><div class="modal-header"><h5 class="modal-title" id="exampleModalLabel">Apagar Item</h5><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button></div><div class="modal-body" id="modal-body"></div><div class="modal-footer"><button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button><a class="btn btn-danger text-white" id="dataConfirmOK">Apagar</a></div></div></div></div>');
+		}
+		$('#modal-body').text(text);
+		$('#dataConfirmOK').attr('href', href);
+		$('#confirm-delete').modal({show: true});
+		return false;
+    });
+    
+});
